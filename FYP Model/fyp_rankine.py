668fa9b72db94e3f344c14bc98552ff502660600
Ng Zhen Bin <zhenbinng@gmail.com>
2024-03-21 11:38:04 +0800

    modify heat exchanger

--- FYP Model/fyp_rankine.py
+++ FYP Model/fyp_rankine.py
@@ -1,3 +1,4 @@
+# Second Trial (boiler as evaporator)
 # %%[sec_1]
 from tespy.networks import Network
 
@@ -7,18 +8,20 @@ my_plant.set_attr(T_unit='C', p_unit='bar', h_unit='kJ / kg')
 # %%[sec_2]
 # import TESPy components
 from tespy.components import (
-    CycleCloser, Pump, Condenser, Turbine, SimpleHeatExchanger, Source, Sink
+    CycleCloser, Pump, Condenser, Turbine, HeatExchanger, Source, Sink
 )
 
 # create components
 cc = CycleCloser('cycle closer')
-sg = SimpleHeatExchanger('steam generator')
+sg = HeatExchanger('steam generator')
 mc = Condenser('main condenser')
 tu = Turbine('steam turbine')
 fp = Pump('feed pump')
 
 cwso = Source('cooling water source')
 cwsi = Sink('cooling water sink')
+stso = Source('steam source')
+stsi = Sink('steam sink')
 
 # Import TESPy connections
 from tespy.connections import Connection
@@ -35,31 +38,33 @@ my_plant.add_conns(c1, c2, c3, c4, c0)
 
 c11 = Connection(cwso, 'out1', mc, 'in2', label='11')
 c12 = Connection(mc, 'out2', cwsi, 'in1', label='12')
+c21 = Connection(stso, 'out1', sg, 'in2', label='21')
+c22 = Connection(sg, 'out2', stsi, 'in1', label='22')
 
-my_plant.add_conns(c11, c12)
+my_plant.add_conns(c11, c12, c21, c22)
 # %%[sec_3]
 # Set component and connection attributes
 mc.set_attr(pr1=1, pr2=0.98)
-sg.set_attr(pr=0.9)
+sg.set_attr(pr1=1, pr2=0.98)
 tu.set_attr(eta_s=0.9)
 fp.set_attr(eta_s=0.75)
 
 c11.set_attr(T=20, p=1.2, fluid={'water': 1})
-c12.set_attr(T=30)
-c1.set_attr(T=600, p=150, m=10, fluid={'water': 1})
-c2.set_attr(p=0.1)
+c12.set_attr(T=25)
+c21.set_attr(T=150, p=2.0, fluid={'water': 1})
+c22.set_attr(T=100)
+c1.set_attr(T=90, p=30, m=10, fluid={'R1234yf': 1})
+c2.set_attr(p=7)
 
 # Solve and print results
 my_plant.solve(mode='design')
 my_plant.print_results()
-# %%[sec_4]
-# Modify attributes, solve again and print results
-mc.set_attr(ttd_u=4)
-c2.set_attr(p=None)
 
-my_plant.solve(mode='design')
-my_plant.print_results()
+# %%[sec_4]
+# Modify attributes (remove how much, replace with how much)
+
 # %%[sec_5]
+# Assess Electrical Power
 from tespy.connections import Bus
 
 # Create a Bus for Electrical Power Output
@@ -77,17 +82,10 @@ my_plant.add_busses(powergen)
 # Solve the network
 my_plant.solve(mode='design')
 my_plant.print_results()
-# %%[sec_6]
-# Modify Attributes for Off-Design Simulation
-powergen.set_attr(P=-10e6)
-c1.set_attr(m=None)
 
-my_plant.solve(mode='design')
-my_plant.print_results()
-# %%[sec_7]
+# %%[sec_6]
 # Parametric Study and Plotting
 my_plant.set_attr(iterinfo=False)
-c1.set_attr(m=20)
 powergen.set_attr(P=None)
 import matplotlib.pyplot as plt
 import numpy as np
@@ -98,63 +96,76 @@ plt.rc('font', **{'size': 18})
 # Step 1a: Setup arrays for different values of live steam temperature, 
 # cooling water temperature, and love steam pressure.
 data = {
-    'T_livesteam': np.linspace(450, 750, 7),
-    'T_cooling': np.linspace(15, 45, 7),
-    'p_livesteam': np.linspace(75, 225, 7)
+    'm_workingfluid': np.linspace(10, 60, 6),
+    'P_boiler': np.linspace(20, 30, 6),
+    'P_condenser': np.linspace(7, 17, 6),
+    'm_coolingfluid': np.linspace(20, 70, 6),
 }
 # Step 1b: Initialize dictionaries ('eta' and 'power') to store efficiency
 # and power values.
 eta = {
-    'T_livesteam': [],
-    'T_cooling': [],
-    'p_livesteam': []
+    'm_workingfluid':[],
+    'P_boiler': [],
+    'P_condenser': [],
+    'm_coolingfluid':[],
 }
 power = {
-    'T_livesteam': [],
-    'T_cooling': [],
-    'p_livesteam': []
+    'm_workingfluid':[],
+    'P_boiler': [],
+    'P_condenser': [],
+    'm_coolingfluid':[],
 }
 # Step 2: Parametric Study Loop
 # Loop over live steam temperature values, set the corresponding attribute,
 # solve the network in design mode, and record the efficiency and power.
-for T in data['T_livesteam']:
-    c1.set_attr(T=T)
+for M in data['m_workingfluid']:
+    c1.set_attr(m=M)
     my_plant.solve('design')
-    eta['T_livesteam'] += [abs(powergen.P.val) / sg.Q.val * 100]
-    power['T_livesteam'] += [abs(powergen.P.val) / 1e6]
+    eta['m_workingfluid'] += [abs(powergen.P.val) / sg.Q.val * 100]
+    power['m_workingfluid'] += [abs(powergen.P.val) / 1e3]
 
-# Step 3: Reset to base temperature
-# Reset the live steam temperature to the base value for the next parametric study.
-c1.set_attr(T=600)
+# Step 3: Reset to base working fluid flow rate
+# Reset the working fluid flow rate to the base value for the next parametric study.
+c1.set_attr(m=10)
 
-# Step 4: Repeat for Cooling Water Temperature
+# Step 4: Repeat for Boiler Pressure
 # Similar loops are used for varying cooling water temperature (T_cooling) 
-for T in data['T_cooling']:
-    c12.set_attr(T=T)
-    c11.set_attr(T=T - 10)
+for P in data['P_boiler']:
+    c1.set_attr(p=P)
     my_plant.solve('design')
-    eta['T_cooling'] += [abs(powergen.P.val) / sg.Q.val * 100]
-    power['T_cooling'] += [abs(powergen.P.val) / 1e6]
+    eta['P_boiler'] += [abs(powergen.P.val) / sg.Q.val * 100]
+    power['P_boiler'] += [abs(powergen.P.val) / 1e3]
 
-# reset to base temperature
-c12.set_attr(T=30)
-c11.set_attr(T=20)
+# reset to base boiler pressure
+c1.set_attr(p=30)
 
-# Step 5: Repeat for Live Steam Pressure
+# Step 5: Repeat for Condenser Pressure
 # Similar loops are used for varying live steam pressure (p_livesteam). 
-for p in data['p_livesteam']:
-    c1.set_attr(p=p)
+for P in data['P_condenser']:
+    c2.set_attr(p=P)
     my_plant.solve('design')
-    eta['p_livesteam'] += [abs(powergen.P.val) / sg.Q.val * 100]
-    power['p_livesteam'] += [abs(powergen.P.val) / 1e6]
+    eta['P_condenser'] += [abs(powergen.P.val) / sg.Q.val * 100]
+    power['P_condenser'] += [abs(powergen.P.val) / 1e3]
 
 # reset to base pressure
-c1.set_attr(p=150)
+c2.set_attr(p=7)
 
-# Step 6: Plotting
+# Step 6: Repeat for Cooling Fluid Flow Rate
+# Similar loops are used for varying live steam pressure (p_livesteam). 
+c12.set_attr(T=None)
+for M in data['m_coolingfluid']:
+    c11.set_attr(m=M)
+    my_plant.solve('design')
+    eta['m_coolingfluid'] += [abs(powergen.P.val) / sg.Q.val * 100]
+    power['m_coolingfluid'] += [abs(powergen.P.val) / 1e3]
+
+# reset to base condition
+c12.set_attr(T=25)
+
+# Step 7: Plotting
 # Plots the results of the parametric study. 
 # Three plots are created for each parameter, showing the relationship between the parameter and efficiency or power.
-fig, ax = plt.subplots(2, 3, figsize=(16, 8), sharex='col', sharey='row')
+fig, ax = plt.subplots(2, 4, figsize=(25, 12.5), sharex='col', sharey='row')
 
 ax = ax.flatten()
 [a.grid() for a in ax]
@@ -162,50 +173,15 @@ ax = ax.flatten()
 i = 0
 for key in data:
     ax[i].scatter(data[key], eta[key], s=100, color="#1f567d")
-    ax[i + 3].scatter(data[key], power[key], s=100, color="#18a999")
+    ax[i + 4].scatter(data[key], power[key], s=100, color="#18a999")
     i += 1
 
-ax[0].set_ylabel('Efficiency in %')
-ax[3].set_ylabel('Power in MW')
-ax[3].set_xlabel('Live steam temperature in °C')
-ax[4].set_xlabel('Feed water temperature in °C')
-ax[5].set_xlabel('Live steam pressure in bar')
+ax[0].set_ylabel('Efficiency (%))')
+ax[4].set_ylabel('Power (kW)')
+ax[4].set_xlabel('Working Fluid Flow Rate (kg/s)')
+ax[5].set_xlabel('Boiler Pressure (bar)')
+ax[6].set_xlabel('Condenser Pressure (bar)')
+ax[7].set_xlabel('Cooling Fluid Flow Rate (kg/s)')
 plt.tight_layout()
-fig.savefig('rankine_parametric.svg')
+fig.savefig('trial2.svg')
 plt.close()
-# %%[sec_8]
-# Set Attributions for Off-Design Simulation
-# Sets specific attributes for off-design simulation. 
-# For example, it sets the main condenser to be off-design with a temperature difference (ttd_u) 
-# and the cooling water mass flow (v) as off-design parameters.
-mc.set_attr(design=["ttd_u"], offdesign=["kA"])
-c11.set_attr(offdesign=["v"])
-c12.set_attr(design=["T"])
-c1.set_attr(design=["p"])
-tu.set_attr(offdesign=["cone"])
-# %%[sec_9]
-# Save the Design for Off-Design Simulation
-# Solves the design case and saves it for later use in off-design simulations.
-my_plant.solve("design")
-my_plant.save("rankine_design")
-# %%[sec_10]
-# Partload Efficiency Loop
-partload_efficiency = []
-partload_m_range = np.linspace(20, 10, 11)
-
-for m in partload_m_range:
-    c1.set_attr(m=m)
-    my_plant.solve("offdesign", design_path="rankine_design")
-    partload_efficiency += [abs(powergen.P.val) / sg.Q.val * 100]
-
-# Plotting Partload Efficiency
-fig, ax = plt.subplots(1, figsize=(16, 8))
-ax.grid()
-ax.scatter(partload_m_range, partload_efficiency, s=100, color="#1f567d")
-ax.set_xlabel("Mass flow in kg/s")
-ax.set_ylabel("Plant electrical efficiency in %")
-
-plt.tight_layout()
-fig.savefig('rankine_partload.svg')
-plt.close()
-# %%[sec_11]
